f03eddae5d1f190aa5715f74c380a535002a05b9
emilhamep <emilhamep@gmail.com>
2017-06-15 11:52:53 +0700

    On progress layout dashboard mahasiswa👨‍🎓

---
+++ controllers/mahasiswaController.js
@@ -0,0 +1,5 @@
+const copyright = "© 2017 Tugas Akhir Dalam Jaringan PPTIK Institut Teknologi Bandung"
+
+exports.dashboard = function(req, res) {
+    return res.render('anggota/mahasiswa/dashboard', { title: 'Dashboard', copyright: copyright })
+}
