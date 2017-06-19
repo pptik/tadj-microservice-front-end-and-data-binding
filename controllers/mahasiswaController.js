a6a6f329e404471303cccdc289963892e7ef5479
emilhamep <emilhamep@gmail.com>
2017-06-19 10:49:26 +0700

    Penyelesaian layout pengaturan akademik sebagai mahasiswa

--- controllers/mahasiswaController.js
+++ controllers/mahasiswaController.js
@@ -3,3 +3,15 @@ const copyright = "© 2017 Tugas Akhir Dalam Jaringan PPTIK Institut Teknologi B
 exports.dashboard = function(req, res) {
     return res.render('anggota/mahasiswa/dashboard', { title: 'Dashboard', copyright: copyright })
 }
+
+exports.pengaturan = function(req, res) {
+    return res.render('anggota/mahasiswa/pengaturan/index', { title: 'Pengaturan', copyright: copyright })
+}
+
+exports.pengaturan_akademik = function(req, res) {
+    return res.render('anggota/mahasiswa/pengaturan/akademik', { title: 'Pengaturan Akademik', copyright: copyright })
+}
+
+exports.pengaturan_profil = function(req, res) {
+    return res.render('anggota/mahasiswa/pengaturan/profil', { title: 'Pengaturan Profil', copyright: copyright })
+}
