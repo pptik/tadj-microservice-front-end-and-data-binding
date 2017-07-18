1b8526f95b5904a1c7c3b70939369b79513c866f
emilhamep <emilhamep@gmail.com>
2017-07-18 13:56:15 +0700

    Layout dosen bimbingan pilih judul dan lakukan bimbingan

--- controllers/dosenController.js
+++ controllers/dosenController.js
@@ -20,30 +20,10 @@ exports.pengaturan_profil_ubah = function(req, res) {
     return res.render('anggota/dosen/pengaturan/profil_ubah', { title: 'Pengaturan Profil', copyright: copyright })
 }
 
-exports.kelompok = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/index', { title: 'Kelompok', copyright: copyright })
-}
-
-exports.pilih_kelompok = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/pilih_kelompok', { title: 'Pilih Kelompok', copyright: copyright })
-}
-
-exports.tambah_kelompok = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/tambah', { title: 'Tambah Kelompok', copyright: copyright })
-}
-
-exports.kelompok_detail = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/detail', { title: 'Detail Kelompok', copyright: copyright })
-}
-
 exports.bimbingan = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/bimbingan', { title: 'Bimbingan', copyright: copyright })
+    return res.render('anggota/dosen/bimbingan/index', { title: 'Pilih judul TA', copyright: copyright })
 }
 
-exports.postervideo = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/postervideo', { title: 'Poster dan Video', copyright: copyright })
-}
-
-exports.ambil_judul = function(req, res) {
-    return res.render('anggota/mahasiswa/kelompok/ambil_judul', { title: 'Ambil Judul Tugas Akhir', copyright: copyright })
+exports.bimbingan_detail = function(req, res) {
+    return res.render('anggota/dosen/bimbingan/detail', { title: 'Detail Bimbingan', copyright: copyright })
 }
